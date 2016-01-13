79fde72b352ff54866574137cd1005254d5980b2
adityakamble49 <adityakamble49@gmail.com>
2016-01-13 11:53:53 +0530

    Background Process Added | Locally Working

---
+++ source/content-based-search-api/src/org/cbsa/api/controller/bgprocess/FileCounterManager.java
@@ -0,0 +1,29 @@
+package org.cbsa.api.controller.bgprocess;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
+
+public class FileCounterManager {
+
+    public static String getCounter() throws IOException {
+        File file = new File("/home/aditya/dataset/counter.txt");
+        Scanner sc = new Scanner(new FileInputStream(file));
+        int count = sc.nextInt();
+        return String.valueOf(count);
+    }
+
+    public static void incCounter() throws IOException {
+        File file = new File("/home/aditya/dataset/counter.txt");
+        Scanner sc = new Scanner(new FileInputStream(file));
+        int count = sc.nextInt();
+        count = count + 1;
+        Writer wr = new FileWriter(file);
+        wr.write(String.valueOf(count));
+        wr.close();
+        System.out.println("Counter Incremented");
+    }
+}
---
+++ source/content-based-search-api/src/org/cbsa/api/controller/bgprocess/MyClient.java
@@ -0,0 +1,39 @@
+package org.cbsa.api.controller.bgprocess;
+
+import java.io.DataInputStream;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class MyClient {
+
+    public static void main(String[] args) {
+        Socket s;
+        try {
+            s = new Socket("localhost", 1111);
+
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            while (true) {
+                String str = dis.readUTF();
+                FileCounterManager.incCounter();
+                // Vector<String> vector = new Vector<String>();
+                ConcurrentLinkedQueue<String> vector = new ConcurrentLinkedQueue<String>();
+                vector.add(str);
+                System.out.println(vector);
+                FileCounterManager.incCounter();
+                System.out.println(str + " Added in Queue");
+                String front = vector.poll();
+                Thread t = new Thread(new WordCount(front));
+                t.start();
+                t.join();
+                // Thread.sleep(10000);
+
+                // vector.remove(front);
+
+                System.out
+                        .println("Metadata Extracted Succuessfully of " + str);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
