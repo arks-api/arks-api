841f923a8e97182adfb0c9559db740833374304d
adityakamble49 <adityakamble49@gmail.com>
2016-03-04 23:00:03 +0530

    Mapper Changed

--- source/content-based-search-api/src/org/cbsa/api/bgmapr/PdfMapper.java
+++ source/content-based-search-api/src/org/cbsa/api/bgmapr/PdfMapper.java
@@ -1,11 +1,11 @@
 package org.cbsa.api.bgmapr;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.cbsa.api.controller.metadata.Stopwords;
 
 public class PdfMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
     private Text word = new Text();
@@ -15,16 +15,16 @@ public class PdfMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
     protected void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
 
-        /*
-         * String line = value.toString().replaceAll("\\.", " ")
-         * .replaceAll("(?!\")\\p{Punct}", "").replaceAll("\"", "")
-         * .replaceAll("[0-9]*", "").toLowerCase(); StringTokenizer tokenizer =
-         * new StringTokenizer(line);
-         *
-         * while (tokenizer.hasMoreTokens()) { word.set(tokenizer.nextToken());
-         * context.progress(); context.write(word, one); }
-         */
+        String line = value.toString().replaceAll("\\.", " ")
+                .replaceAll("(?!\")\\p{Punct}", "").replaceAll("\"", "")
+                .replaceAll("[0-9]*", "").toLowerCase();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+            word.set(tokenizer.nextToken());
+            context.write(word, one);
+        }
 
+        /*
         String text = value.toString();
         String[] words = text.replaceAll("\\p{Punct}|\\d", "").split(" ");
         for (String singleWord : words) {
@@ -34,5 +34,6 @@ public class PdfMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
                 context.write(word, one);
             }
         }
+        */
     }
 }
