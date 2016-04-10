dea36be1b3734034f76ef44ee227ca604c90778c
adityakamble49 <adityakamble49@gmail.com>
2016-04-10 14:45:29 +0530

    Hadoop Maven Project sample added

---
+++ modules-samples/maven-example/MavenSample/src/main/java/org/example/hadoop/MavenSample/App.java
@@ -0,0 +1,13 @@
+package org.example.hadoop.MavenSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
